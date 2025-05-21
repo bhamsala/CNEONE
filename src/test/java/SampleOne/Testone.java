2da62f7d233d3dc9174768abe4a6474a69bcefd3
bhamsala <bhargava.hamsala@gmail.com>
2025-05-21 18:40:37 -0500

    First Program

---
+++ src/test/java/SampleOne/Testone.java
@@ -0,0 +1,10 @@
+package SampleOne;
+
+public class Testone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Welcome to One May21");
+	}
+
+}
