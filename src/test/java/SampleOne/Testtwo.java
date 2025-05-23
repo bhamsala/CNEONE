e1ed868fe694efcb42ca6004512a632ec86cd5f5
Abhihamsala <abhihamsala@gmail.com>
2025-05-22 22:13:36 -0500

    first updated abhi program

---
+++ src/test/java/SampleOne/Testtwo.java
@@ -0,0 +1,11 @@
+package SampleOne;
+
+public class Testtwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to update");
+		System.out.println("Welcome to new");
+	}
+
+}
